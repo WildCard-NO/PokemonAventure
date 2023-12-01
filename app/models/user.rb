bf0885896283006a03b89d778cd6f1559602abca
WildCard-NO <kalex.boe@gmail.com>
2023-12-01 09:55:55 +0100

    Create User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# email:string
+# password_Digest:string
+#
+# password:string virtual_attribute
+# password_confirmation:string virtual_attribute
 class User < ApplicationRecord
+    has_secure_password
 end
