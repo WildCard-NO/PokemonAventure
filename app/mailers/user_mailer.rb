a3fb893309abffbcf364cbc79007312c1dc978d4
WildCard-NO <kalex.boe@gmail.com>
2023-12-06 15:16:11 +0100

    Reset Password

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,24 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.Password.subject
+  #
+  def Password
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset.subject
+  #
+  def reset
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
